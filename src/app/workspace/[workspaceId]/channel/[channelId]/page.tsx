6a2d95b9f4b4eb2dc2f2ceb3cb477c40074a20b8
Daanish2003 <redchillies6786@gmail>
2024-10-14 19:07:13 +0530

    channel editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import ChatInput from "@/features/channels/components/chat-input";
 import Header from "@/features/channels/components/header";
 import { useChannelId } from "@/hooks/use-channel-id"
 import { Loader, TriangleAlert } from "lucide-react";
@@ -32,6 +33,10 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
         <Header title={channel.name} />
+        <div className="flex-1"/>
+        <ChatInput 
+        placeholder={`Message # ${channel.name}`}
+        />
     </div>
   )
 }
